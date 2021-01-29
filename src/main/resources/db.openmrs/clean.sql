aff15c0bc97b8c688d3d9cf7efa7ff4a591b1fa5
Vivek Singh <petmongrels@gmail.com>
2021-01-29 14:13:12 +0530

    vivek | moving ashwini specific code to impl repository

--- src/main/resources/db.openmrs/clean.sql
+++
@@ -1,5 +0,0 @@
-delete from concept_name where date_created > '2020-12-01';
-delete from concept where date_created > '2020-12-01';
-delete from encounter_type where date_created > '2020-12-01';
-
-DROP PROCEDURE IF EXISTS add_concept_abi;
