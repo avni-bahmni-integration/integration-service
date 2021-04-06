fb82aa1834d3f4f1ce4d34b162d74a92a6b68479
Hiren Thacker <hithacker@gmail.com>
2021-04-06 13:07:58 +0530

    Added CLI argument for migration direction(avni-to-bahmni or bahmni-to-avni)

--- metadata-migrator/src/main/java/org/bahmni_avni_integration/migrator/Migrator.java
+++ metadata-migrator/src/main/java/org/bahmni_avni_integration/migrator/Migrator.java
@@ -1,9 +1,9 @@
 package org.bahmni_avni_integration.migrator;
 
 import org.apache.log4j.Logger;
+import org.bahmni_avni_integration.migrator.service.AvniToBahmniService;
 import org.bahmni_avni_integration.migrator.service.BahmniToAvniService;
 import org.bahmni_avni_integration.migrator.service.IntegrationDataService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,12 +19,17 @@ import java.util.stream.Collectors;
 @EnableJpaRepositories("org.bahmni_avni_integration.integration_data.repository")
 @EntityScan(basePackages = {"org.bahmni_avni_integration.integration_data.*", "org.bahmni_avni_integration.integration_data.domain.*"})
 public class Migrator implements CommandLineRunner {
-    @Autowired
-    private BahmniToAvniService bahmniToAvniService;
-    @Autowired
-    private IntegrationDataService integrationDataService;
+    private final BahmniToAvniService bahmniToAvniService;
+    private final IntegrationDataService integrationDataService;
+    private final AvniToBahmniService avniToBahmniService;
 
     private static final Logger logger = Logger.getLogger(Migrator.class);
+
+    public Migrator(BahmniToAvniService bahmniToAvniService, IntegrationDataService integrationDataService, AvniToBahmniService avniToBahmniService) {
+        this.bahmniToAvniService = bahmniToAvniService;
+        this.integrationDataService = integrationDataService;
+        this.avniToBahmniService = avniToBahmniService;
+    }
 
     public static void main(String[] args) throws SQLException {
         SpringApplication.run(Migrator.class, args).close();
@@ -34,11 +39,26 @@ public class Migrator implements CommandLineRunner {
     public void run(String... args) throws Exception {
         List<String> nonSpringArguments = Arrays.stream(args).filter(s -> !s.startsWith("--")).collect(Collectors.toList());
         if (nonSpringArguments.size() == 0) return;
-        if (nonSpringArguments.get(0).equals("adhoc")) {
-            runAdhoc();
-            System.exit(0);
+        switch (nonSpringArguments.get(0)) {
+            case "adhoc" -> {
+                runAdhoc();
+            }
+            case "bahmni-to-avni" -> {
+                bahmniToAvni();
+            }
+            case "avni-to-bahmni" -> {
+                avniToBahmni();
+            }
         }
 
+    }
+
+    private void avniToBahmni() {
+        logger.debug("Migrating metadata from Avni to Bahmni");
+        System.exit(0);
+    }
+
+    private void bahmniToAvni() {
         try {
             bahmniToAvniService.cleanup();
             integrationDataService.cleanup();
@@ -60,5 +80,6 @@ public class Migrator implements CommandLineRunner {
     private void runAdhoc() {
         integrationDataService.cleanupConstants();
         integrationDataService.createConstants();
+        System.exit(0);
     }
 }
