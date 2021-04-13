432a782eb646b9554ba0e5177887eeba10ed8ff2
Hiren Thacker <hithacker@gmail.com>
2021-04-13 13:17:00 +0530

    Remove unused code in EnrolmentService

--- integrator/src/main/java/org/bahmni_avni_integration/service/EnrolmentService.java
+++ integrator/src/main/java/org/bahmni_avni_integration/service/EnrolmentService.java
@@ -49,8 +49,6 @@ public class EnrolmentService {
 
     public OpenMRSFullEncounter createCommunityEnrolment(Enrolment enrolment, OpenMRSUuidHolder openMRSPatient, Constants constants) {
         OpenMRSUuidHolder visit = visitService.getOrCreateVisit(openMRSPatient);
-        MappingMetaDataCollection encounterTypes = mappingMetaDataRepository.findAll(MappingGroup.ProgramEnrolment, MappingType.CommunityEnrolment_EncounterType);
-        String encounterTypeUuid = encounterTypes.getBahmniValueForAvniValue(enrolment.getProgram());
         OpenMRSEncounter encounter = enrolmentMapper.mapEnrolmentToEncounter(enrolment, openMRSPatient.getUuid(), constants);
         encounter.setVisit(visit.getUuid());
         OpenMRSFullEncounter savedEncounter = openMRSEncounterRepository.createEncounter(encounter);
