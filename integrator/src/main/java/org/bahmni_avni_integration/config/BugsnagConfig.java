adaedf376b15a10686b7ba178b6d49fcf3b247f4
Vivek Singh <petmongrels@gmail.com>
2021-04-20 12:50:06 +0530

    bugsnag setup done

--- integrator/src/main/java/org/bahmni_avni_integration/config/BugsnagConfig.java
+++ integrator/src/main/java/org/bahmni_avni_integration/config/BugsnagConfig.java
@@ -2,6 +2,7 @@ package org.bahmni_avni_integration.config;
 
 import com.bugsnag.Bugsnag;
 import com.bugsnag.BugsnagSpringConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -9,8 +10,11 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import(BugsnagSpringConfiguration.class)
 public class BugsnagConfig {
+    @Value("${bugsnag.api.key}")
+    private String apiKey;
+
     @Bean
     public Bugsnag bugsnag() {
-        return new Bugsnag("your-api-key-here");
+        return new Bugsnag(apiKey);
     }
 }
