daa613dd6f0602c8e3478554b93ae6693898a748
Vivek Singh <petmongrels@gmail.com>
2021-06-21 13:42:44 +0530

    scripts for installation and running on production servers

---
+++ scripts/install-jdk.sh
@@ -0,0 +1,4 @@
+cd /tmp/
+wget --no-check-certificate https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk-15.0.2%2B7/OpenJDK15U-jdk_x64_linux_hotspot_15.0.2_7.tar.gz
+cd /usr/java
+sudo tar -xvzf /tmp/OpenJDK15U-jdk_x64_linux_hotspot_15.0.2_7.tar.gz
