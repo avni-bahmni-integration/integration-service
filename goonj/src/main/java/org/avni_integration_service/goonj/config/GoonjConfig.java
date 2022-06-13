451a2fdd92f8881effac90ab101d8eb565ab11fe
himeshr <himeshl@samanvayfoundation.org>
2022-06-13 13:22:04 +0530

    Ensure the goonj oauthToken is refreshed only after expiry time period

--- goonj/src/main/java/org/avni_integration_service/goonj/config/GoonjConfig.java
+++ goonj/src/main/java/org/avni_integration_service/goonj/config/GoonjConfig.java
@@ -4,13 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -34,8 +30,11 @@ public class GoonjConfig {
     @Value("${goonj.sf.appUrl}")
     private String appUrl;
 
+    @Value("${goonj.sf.tokenExpiry}")
+    private int tokenExpiry;
+
     @Autowired
-    private SalesForceUserRepository salesForceUserRepository;
+    private TokenService tokenService;
 
     public String getSalesForceAuthUrl() {
         return salesForceAuthUrl;
@@ -61,15 +60,16 @@ public class GoonjConfig {
         return appUrl;
     }
 
+    public int getTokenExpiry() {
+        return tokenExpiry;
+    }
+
     @Bean("GoonjRestTemplate")
     RestTemplate restTemplate(OAuth2AuthorizedClientService clientService) {
         return new RestTemplateBuilder()
                 .interceptors((ClientHttpRequestInterceptor) (httpRequest, bytes, execution) -> {
-                //TODO , do token fetch only on first attempt or if it has expired
-
-                httpRequest.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer " + salesForceUserRepository
-                        .login().getAccessToken());
-
+                    httpRequest.getHeaders().add(HttpHeaders.AUTHORIZATION,
+                            "Bearer " + tokenService.getRefreshedToken().getTokenValue());
                     return execution.execute(httpRequest, bytes);
                 })
                 .build();
