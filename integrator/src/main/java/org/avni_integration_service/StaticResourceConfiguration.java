a78c42ea64960e876e2fa07ba489b3f19542f116
himeshr <himeshl@samanvayfoundation.org>
2022-08-26 18:11:46 +0530

    Fix issue with int-service apis returning xml content instead of json type content

--- integrator/src/main/java/org/avni_integration_service/StaticResourceConfiguration.java
+++ integrator/src/main/java/org/avni_integration_service/StaticResourceConfiguration.java
@@ -1,10 +1,15 @@
 package org.avni_integration_service;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.CacheControl;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
@@ -43,6 +48,14 @@ public class StaticResourceConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/avni-int-admin-app/")
                 .setViewName("forward:/avni-int-admin-app/index.html");
+    }
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        final MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        converter.setObjectMapper(objectMapper);
+        converters.add(converter);
     }
 
 }
