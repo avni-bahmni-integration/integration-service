eefbe1c2321afc6f72a329e92bb2c800e83aa3f3
himeshr <himeshl@samanvayfoundation.org>
2022-10-12 11:51:13 +0530

    #47 | Add errorClassifier test cases for Known Goonj FieldCustomValidation errors

--- integration-data/src/test/java/org/avni_integration_service/integration_data/service/error/ErrorClassifierForGoonjTestConstants.java
+++ integration-data/src/test/java/org/avni_integration_service/integration_data/service/error/ErrorClassifierForGoonjTestConstants.java
@@ -5,7 +5,13 @@ public interface ErrorClassifierForGoonjTestConstants {
     String STRING_FORMAT_BUFFETED_ERROR_MSG = "<<Head -- %s -- tail>>";
     String ERROR_MSG_STANDARD_SKIP = "aafafasdg adfad1232r @SDA!2CGAE7670$%.!";
     String ERROR_MSG_DISPATCH_MISSING_DEMAND = "Individual not found with UUID 'null' or External ID 'a1CHE0000001qqz2AA'";
-    String ERROR_MSG_INVALID_VALUE_FOR_RESTRICTED_PICKLIST = "INVALID_OR_NULL_FOR_RESTRICTED_PICKLIST";
+    String ERROR_MSG_INVALID_VALUE_FOR_RESTRICTED_PICKLIST = "INVALID_OR_NULL_FOR_RESTRICTED_PICKLIST, District: bad value for restricted picklist field";
     String ERROR_MSG_DISPATCH_RECEIPT_DUPLICATE = "System.ListException: Before Insert or Upsert list must not have two identically equal elements";
     String ERROR_MSG_FIELD_CUSTOM_VALIDATION_EXCEPTION = "FIELD_CUSTOM_VALIDATION_EXCEPTION";
+    String ERROR_MSG_DISPATCH_RECEIPT_INVALID_RECEIVED_DATE = "FIELD_CUSTOM_VALIDATION_EXCEPTION, Received Date can not be prior to related DispatchDtae";
+    String ERROR_MSG_DISPATCH_RECEIPT_DUPLICATES = "FIELD_CUSTOM_VALIDATION_EXCEPTION, More than one Dispatch Received Status cannot be created for a Dispatch Status.";
+    String ERROR_MSG_DISPATCH_RECEIPT_LINE_ITEM_MISMATCH = "FIELD_CUSTOM_VALIDATION_EXCEPTION, Material Received can not be inserted or updated as Related Dispatch Status have no Line Item";
+    String ERROR_MSG_DISTRIBUTION_DISASTER_MISSING = "FIELD_CUSTOM_VALIDATION_EXCEPTION, Please fill in the Disaster Type, if the Type of Initiative contains Rahat.";
+    String ERROR_MSG_ACTIVITY_MEASUREMENT_TYPE_MISSING = "FIELD_CUSTOM_VALIDATION_EXCEPTION, Please fill in Measurement Type.";
+
 }
--- integration-data/src/test/java/org/avni_integration_service/integration_data/service/error/ErrorClassifierForTest.java
+++ integration-data/src/test/java/org/avni_integration_service/integration_data/service/error/ErrorClassifierForTest.java
@@ -47,6 +47,31 @@ public class ErrorClassifierForTest extends AbstractSpringTest implements ErrorC
                 assertNotNull(invokeClassifyForContains(ERROR_MSG_FIELD_CUSTOM_VALIDATION_EXCEPTION));
         }
 
+        @Test
+        public void classifyDispatchReceiptInvalidReceivedDateError() {
+                assertNotNull(invokeClassifyForContains(ERROR_MSG_DISPATCH_RECEIPT_INVALID_RECEIVED_DATE));
+        }
+
+        @Test
+        public void classifyDispatchReceiptDuplicatesError() {
+                assertNotNull(invokeClassifyForContains(ERROR_MSG_DISPATCH_RECEIPT_DUPLICATES));
+        }
+
+        @Test
+        public void classifyErrorMsgDispatchReceiptLineItemMismatchError() {
+                assertNotNull(invokeClassifyForContains(ERROR_MSG_DISPATCH_RECEIPT_LINE_ITEM_MISMATCH));
+        }
+
+        @Test
+        public void classifyErrorMsgDistributionDisasterMissingError() {
+                assertNotNull(invokeClassifyForContains(ERROR_MSG_DISTRIBUTION_DISASTER_MISSING));
+        }
+
+        @Test
+        public void classifyErrorMsgActivityMeasurementTypeMissingError() {
+                assertNotNull(invokeClassifyForContains(ERROR_MSG_ACTIVITY_MEASUREMENT_TYPE_MISSING));
+        }
+
         private ErrorType invokeClassifyForContains(String errorMessage) {
                 String buffetedMsg = String.format(STRING_FORMAT_BUFFETED_ERROR_MSG, errorMessage);
                 return errorClassifier.classify(integrationSystem, buffetedMsg);
