e2958b7d3cc11d94b68b42aadb0ca6efdd3108c0
Vivek Singh <petmongrels@gmail.com>
2023-05-12 21:42:16 +0530

    #66 - external test to check keycloak call

---
+++ avni/src/test/java/org/avni_integration_service/avni/client/AvniSessionET.java
@@ -0,0 +1,24 @@
+package org.avni_integration_service.avni.client;
+
+import org.avni_integration_service.avni.domain.auth.IdpDetailsResponse;
+import org.avni_integration_service.avni.domain.auth.KeycloakDetails;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AvniSessionET {
+    @Test
+    void getIdToken() {
+        AvniSession avniSession = new AvniSession("http://localhost:8021", "vin@jsscp", "password", true, IdpType.Keycloak);
+        IdpDetailsResponse idpDetailsResponse = new IdpDetailsResponse();
+        KeycloakDetails keycloak = new KeycloakDetails();
+        keycloak.setAuthServerUrl("http://localhost:8080");
+        keycloak.setClientId("avni-client");
+        keycloak.setGrantType("password");
+        keycloak.setRealm("On-premise");
+        keycloak.setScope("openid");
+        idpDetailsResponse.setKeycloak(keycloak);
+        String idToken = avniSession.fetchIdToken(idpDetailsResponse);
+    }
+}
