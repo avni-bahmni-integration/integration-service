291fc6c2d927a36a269dbc0ebbcda09f65cd6838
Vivek Singh <petmongrels@gmail.com>
2023-07-18 23:00:53 +0530

    #72 - return type for UI

--- integrator/src/main/java/org/avni_integration_service/web/contract/IntegrationSystemContract.java
+++ integrator/src/main/java/org/avni_integration_service/web/contract/IntegrationSystemContract.java
@@ -3,9 +3,9 @@ package org.avni_integration_service.web.contract;
 import org.avni_integration_service.integration_data.domain.IntegrationSystem;
 
 public class IntegrationSystemContract {
-    private IntegrationSystem.IntegrationSystemType type;
-    private int id;
-    private String instanceName;
+    private final IntegrationSystem.IntegrationSystemType type;
+    private final int id;
+    private final String instanceName;
 
     public IntegrationSystemContract(IntegrationSystem integrationSystem) {
         this.type = integrationSystem.getSystemType();
